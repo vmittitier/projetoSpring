13c0c6a6df8a2d3e92714b7fe3372f701dd8fe80
vini <mitnewzealand@gmail.com>
2019-10-28 15:23:24 -0300

    spring start

---
+++ src/test/java/com/invillia/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
